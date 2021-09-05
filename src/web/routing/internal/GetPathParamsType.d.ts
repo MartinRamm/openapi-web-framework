5d3fff571a375a9da90c6d55a1949ec13bf52975
Martin Ramm <martin.ramm@flexperto.com>
2021-09-05 13:22:34 +0200

    Add more test cases for GetPathParamsType and make them pass

--- src/web/routing/internal/GetPathParamsType.d.ts
+++ src/web/routing/internal/GetPathParamsType.d.ts
@@ -1,40 +1,29 @@
-export type GetPathParamsType<T extends string> = T extends `\\{${infer Right}`
-  ? GetPathParamsType<Right>
+type ExtractPathParams<T extends string> = T extends `\\{${infer Right}`
+  ? ExtractPathParams<Right>
   : T extends `\\}${infer Right}`
-  ? GetPathParamsType<Right>
+  ? ExtractPathParams<Right>
   : T extends `${infer Left}\\{`
-  ? GetPathParamsType<Left>
+  ? ExtractPathParams<Left>
   : T extends `${infer Left}\\}`
-  ? GetPathParamsType<Left>
+  ? ExtractPathParams<Left>
   : T extends `${infer Left}\\{${infer CenterL}{${infer Param}}${infer CenterR}\\}${infer Right}` // 'Left\\{CenterL{Param}CenterR\\}Right'
-  ? GetPathParamsType<Left> | GetPathParamsType<CenterL> | Param | GetPathParamsType<CenterR> | GetPathParamsType<Right>
+  ? ExtractPathParams<Left> | ExtractPathParams<CenterL> | Param | ExtractPathParams<CenterR> | ExtractPathParams<Right>
   : T extends `${infer Left}{${infer ParamA}\\{${infer ParamB}\\}${infer ParamC}}${infer Right}` // 'Left{ParamA\\{ParamB\\}ParamC}Right'
-  ?
-      | (ParamA extends `${infer ParamAL}}${infer ParamAR}`
-          ? GetPathParamsType<`{${ParamAL}}`> | GetPathParamsType<`${ParamAR}\\{${ParamB}\\}${ParamC}}`>
-          : `${ParamA}{${ParamB}}${ParamC}`)
-      | GetPathParamsType<Left>
-      | GetPathParamsType<Right>
+  ? `${ParamA}{${ParamB}}${ParamC}` | ExtractPathParams<Left> | ExtractPathParams<Right>
   : T extends `${infer Left}\\{${infer Center}\\}${infer Right}` // 'Left\\{Center\\}Right'
-  ? GetPathParamsType<Left> | GetPathParamsType<Center> | GetPathParamsType<Right>
+  ? ExtractPathParams<Left> | ExtractPathParams<Center> | ExtractPathParams<Right>
   : T extends `${infer Left}{${infer ParamL}\\}${infer ParamR}}${infer Right}` // 'Left{ParamL\\}ParamR}Right'
-  ?
-      | (ParamL extends `${infer ParamLL}}${infer ParamLR}`
-          ? GetPathParamsType<`{${ParamLL}}`> | GetPathParamsType<`${ParamLR}\\}${ParamR}}`>
-          : `${ParamL}}${ParamR}`)
-      | GetPathParamsType<Left>
-      | GetPathParamsType<Right>
+  ? `${ParamL}}${ParamR}` | ExtractPathParams<Left> | ExtractPathParams<Right>
   : T extends `${infer Left}{${infer ParamL}\\{${infer ParamR}}${infer Right}` // 'Left{ParamL\\{ParamR}Right'
-  ?
-      | (ParamL extends `${infer ParamLL}}${infer ParamLR}`
-          ? GetPathParamsType<`{${ParamLL}}`> | GetPathParamsType<`${ParamLR}\\{${ParamR}}`>
-          : `${ParamL}{${ParamR}`)
-      | GetPathParamsType<Left>
-      | GetPathParamsType<Right>
+  ? `${ParamL}{${ParamR}` | ExtractPathParams<Left> | ExtractPathParams<Right>
   : T extends `${infer Left}\\{${infer Right}` // 'Left\\{Right'
-  ? GetPathParamsType<Left> | GetPathParamsType<Right>
+  ? ExtractPathParams<Left> | ExtractPathParams<Right>
   : T extends `${infer Left}\\}${infer Right}` // 'Left\\}Right'
-  ? GetPathParamsType<Left> | GetPathParamsType<Right>
+  ? ExtractPathParams<Left> | ExtractPathParams<Right>
   : T extends `${infer Left}{${infer Param}}${infer Right}` // 'Left{Param}Right'
-  ? Param | GetPathParamsType<Left> | GetPathParamsType<Right>
+  ? Param | ExtractPathParams<Left> | ExtractPathParams<Right>
   : never;
+
+export type GetPathParamsType<T extends string> =
+  //analyze path segment by path segment
+  T extends `${infer Left}/${infer Right}` ? GetPathParamsType<Left> | GetPathParamsType<Right> : ExtractPathParams<T>;
--- test/shared/web/routing/internal/pathParamsTestCases.js
+++ test/shared/web/routing/internal/pathParamsTestCases.js
@@ -99,43 +99,93 @@ addTestCaseWithExtraParams(
 );
 
 addTestCaseWithExtraParams('param name containing escaped } in middle', '{as\\}df}', ['as}df']);
-addTestCaseWithExtraParams('2 param name containing escaped } in middle', '{as\\}df}/{gh\\}ij}', ['as}df', 'gh}ij']);
+addTestCaseWithExtraParams('2 param names containing escaped } in middle', '{as\\}df}/{gh\\}ij}', ['as}df', 'gh}ij']);
+addTestCaseWithExtraParams(
+  '2 param names seperated by a simple param containing escaped } in middle',
+  '{as\\}df}/{_}/{gh\\}ij}',
+  ['as}df', '_', 'gh}ij']
+);
 
 addTestCaseWithExtraParams('param name containing escaped } at end', '{asdf\\}}', ['asdf}']);
-addTestCaseWithExtraParams('2 param name containing escaped } at end', '{asdf\\}}/{ghij\\}}', ['asdf}', 'ghij}']);
+addTestCaseWithExtraParams('2 param names containing escaped } at end', '{asdf\\}}/{ghij\\}}', ['asdf}', 'ghij}']);
+addTestCaseWithExtraParams(
+  '2 param names seperated by a simple param containing escaped } at end',
+  '{asdf\\}}/{_}/{ghij\\}}',
+  ['asdf}', '_', 'ghij}']
+);
 
 addTestCaseWithExtraParams('param name containing escaped } at start', '{\\}asdf}', ['}asdf']);
-addTestCaseWithExtraParams('2 param name containing escaped } at start', '{\\}asdf}/{\\}ghij}', ['}asdf', '}ghij']);
+addTestCaseWithExtraParams('2 param names containing escaped } at start', '{\\}asdf}/{\\}ghij}', ['}asdf', '}ghij']);
+addTestCaseWithExtraParams(
+  '2 param names seperated by a simple param containing escaped } at start',
+  '{\\}asdf}/{_}/{\\}ghij}',
+  ['}asdf', '_', '}ghij']
+);
 
 addTestCaseWithExtraParams('param name containing escaped { in middle', '{as\\{df}', ['as{df']);
-addTestCaseWithExtraParams('2 param name containing escaped { in middle', '{as\\{df}/{gh\\{ij}', ['as{df', 'gh{ij']);
+addTestCaseWithExtraParams('2 param names containing escaped { in middle', '{as\\{df}/{gh\\{ij}', ['as{df', 'gh{ij']);
+addTestCaseWithExtraParams(
+  '2 param names seperated by a simple param containing escaped { in middle',
+  '{as\\{df}/{_}/{gh\\{ij}',
+  ['as{df', '_', 'gh{ij']
+);
 
 addTestCaseWithExtraParams('param name containing escaped { at end', '{asdf\\{}', ['asdf{']);
-addTestCaseWithExtraParams('2 param name containing escaped { at end', '{asdf\\{}/{ghij\\{}', ['asdf{', 'ghij{']);
+addTestCaseWithExtraParams('2 param names containing escaped { at end', '{asdf\\{}/{ghij\\{}', ['asdf{', 'ghij{']);
+addTestCaseWithExtraParams(
+  '2 param names seperated by a simple param containing escaped { at end',
+  '{asdf\\{}/{_}/{ghij\\{}',
+  ['asdf{', '_', 'ghij{']
+);
 
 addTestCaseWithExtraParams('param name containing escaped { at start', '{\\{asdf}', ['{asdf']);
-addTestCaseWithExtraParams('2 param name containing escaped { at start', '{\\{asdf}/{\\{ghij}', ['{asdf', '{ghij']);
+addTestCaseWithExtraParams('2 param names containing escaped { at start', '{\\{asdf}/{\\{ghij}', ['{asdf', '{ghij']);
+addTestCaseWithExtraParams(
+  '2 param names seperated by a simple param containing escaped { at start',
+  '{\\{asdf}/{_}/{\\{ghij}',
+  ['{asdf', '_', '{ghij']
+);
 
 addTestCaseWithExtraParams('param name containing escaped { and }', '{\\{as\\}}', ['{as}']);
-addTestCaseWithExtraParams('2 param name containing escaped { and }', '{\\{as\\}}/{\\{df\\}}', ['{as}', '{df}']);
+addTestCaseWithExtraParams('2 param names containing escaped { and }', '{\\{as\\}}/{\\{df\\}}', ['{as}', '{df}']);
+addTestCaseWithExtraParams(
+  '2 param names seperated by a simple param containing escaped { and }',
+  '{\\{as\\}}/{_}/{\\{df\\}}',
+  ['{as}', '_', '{df}']
+);
 
 addTestCaseWithExtraParams('param name containing escaped { and } with prefix', '{z\\{as\\}}', ['z{as}']);
-addTestCaseWithExtraParams('2 param name containing escaped { and } with prefix', '{z\\{as\\}}/{z\\{df\\}}', [
+addTestCaseWithExtraParams('2 param names containing escaped { and } with prefix', '{z\\{as\\}}/{z\\{df\\}}', [
   'z{as}',
   'z{df}',
 ]);
+addTestCaseWithExtraParams(
+  '2 param names separated by a simple param containing escaped { and } with prefix',
+  '{z\\{as\\}}/{_}/{z\\{df\\}}',
+  ['z{as}', '_', 'z{df}']
+);
 
 addTestCaseWithExtraParams('param name containing escaped { and } with postfix', '{\\{as\\}z}', ['{as}z']);
-addTestCaseWithExtraParams('2 param name containing escaped { and } with postfix', '{\\{as\\}z}/{\\{df\\}z}', [
+addTestCaseWithExtraParams('2 param names containing escaped { and } with postfix', '{\\{as\\}z}/{\\{df\\}z}', [
   '{as}z',
   '{df}z',
 ]);
+addTestCaseWithExtraParams(
+  '2 param names separated by a simple param containing escaped { and } with postfix',
+  '{\\{as\\}z}/{_}/{\\{df\\}z}',
+  ['{as}z', '_', '{df}z']
+);
 
 addTestCaseWithExtraParams('param name containing escaped { and } with pre- and postfix', '{z\\{as\\}z}', ['z{as}z']);
 addTestCaseWithExtraParams(
-  '2 param name containing escaped { and } with pre- and postfix',
+  '2 param names containing escaped { and } with pre- and postfix',
   '{z\\{as\\}z}/{z\\{df\\}z}',
   ['z{as}z', 'z{df}z']
+);
+addTestCaseWithExtraParams(
+  '2 param names separated by a simple param containing escaped { and } with pre- and postfix',
+  '{z\\{as\\}z}/{_}/{z\\{df\\}z}',
+  ['z{as}z', '_', 'z{df}z']
 );
 
 module.exports = { pathParamsTestCases: testCases };
