3536bc0a70bcb8f9f181124b4b62f2e4b77de2b8
Martin Ramm <martin.ramm@flexperto.com>
2021-09-08 19:51:05 +0200

    Add linting to unit tests via `eslint-plugin-jest`

---
+++ test/unit/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: false,
+  extends: [
+    'plugin:jest/all'
+  ],
+
+  rules: {
+    'jest/valid-title': 'off',
+    'jest/prefer-expect-assertions': 'off',
+    'jest/consistent-test-it': [
+      'error',
+      {
+        'fn': 'test',
+        'withinDescribe': 'test'
+      }
+    ],
+    'no-restricted-properties': ['error', {
+      'object': 'test',
+      'property': 'each',
+      'message': 'Please use `[...].forEach((each) => { test(...) });` instead, as `test.each` is not typed.'
+    }]
+  }
+}
