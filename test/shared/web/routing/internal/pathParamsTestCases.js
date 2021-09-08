7c6d4026000023894d8c9c709757099992c7a744
Martin Ramm <martin.ramm@flexperto.com>
2021-09-08 19:50:23 +0200

    Add more test cases

--- test/shared/web/routing/internal/pathParamsTestCases.js
+++ test/shared/web/routing/internal/pathParamsTestCases.js
@@ -188,4 +188,10 @@ addTestCaseWithExtraParams(
   ['z{as}z', '_', 'z{df}z']
 );
 
+addTestCaseWithExtraParams(
+  'Complex case with lots of parameters',
+  '\\}a/\\{b/\\{c\\}/\\{{d}\\}/\\{e{f}\\}/\\{{g}h\\}/\\{i{j}k\\}/{l\\}m}/{n\\}}/{\\}o}/{p\\{q}/{r\\{}/{\\{s}/{\\{t\\}}/{u\\{v\\}}/{\\{w\\}x}/{y\\{z\\}0}',
+  ['d', 'f', 'g', 'j', 'l}m', 'n}', '}o', 'p{q', 'r{', '{s', '{t}', 'u{v}', '{w}x', 'y{z}0']
+);
+
 module.exports = { pathParamsTestCases: testCases };
--- test/unit/util/regexpLastIndexOf.ts
+++ test/unit/util/regexpLastIndexOf.ts
@@ -1,17 +1,22 @@
 import { describe, expect, test } from '@jest/globals';
 import { regexLastIndexOf } from '../../../src/util/regexpLastIndexOf';
 
+const input = 'asdfgasdfasdfasdfga';
 describe('regexpLastIndexOf', () => {
-  test.each([
-    { name: 0, input: 'asdfasdfasdf', search: 'asdfasdfasdf', expected: 0 },
-    { name: 1, input: 'asdfasdfasdf', search: 'asdf', expected: 8 },
-    { name: 2, input: 'asdfasdfasdf', search: 'a', expected: 8 },
-    { name: 3, input: 'asdfasdfasdf', search: 's', expected: 9 },
-    { name: 4, input: 'asdfasdfasdf', search: 'd', expected: 10 },
-    { name: 5, input: 'asdfasdfasdf', search: 'f', expected: 11 },
-    { name: 6, input: 'asdfasdfasdf', search: 'g', expected: -1 },
-  ])('$name', ({ input, search, expected }) => {
-    const actual = regexLastIndexOf(input, new RegExp(search));
-    expect(expected).toEqual(actual);
+  [
+    { search: new RegExp(input), expected: 0 },
+    { search: /(?<=g)a/, expected: 18 }, //positive lookbehind
+    { search: /(?<!g)a/, expected: 13 }, //negative lookbehind
+    { search: /asdf/, expected: 13 },
+    { search: /a/, expected: 18 },
+    { search: /s/, expected: 14 },
+    { search: /d/, expected: 15 },
+    { search: /f/, expected: 16 },
+    { search: /h/, expected: -1 },
+  ].forEach(({ search, expected }) => {
+    test(search.toString(), () => {
+      const actual = regexLastIndexOf(input, search);
+      expect(actual).toStrictEqual(expected);
+    });
   });
 });
